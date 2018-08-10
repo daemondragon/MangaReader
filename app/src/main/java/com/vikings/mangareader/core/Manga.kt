b5f8608a36d13fb509c50f7f4fb6d2e5d0678cd8
jeremy beuvry <jeremy.beuvry@epita.fr>
2018-08-10 22:21:38 +0200

    add Manga interface

---
+++ app/src/main/java/com/vikings/mangareader/core/Manga.kt
@@ -0,0 +1,86 @@
+package com.vikings.mangareader.core
+
+/**
+ * Contains all information relative to the manga.
+ *
+ * Some fields are nullable as they can be empty if an
+ * error occurs during information retrieval (network errors...),
+ * and at some point, they can't be loaded yet.
+ *
+ * For all fields that are a list, an empty list means that there is nothing
+ * in it (content loaded), whereas a null list means that the field is not
+ * yet loaded (or an error occurred).
+ *
+ * A Manga is considered the same as another one if they share the same [name]
+ * and the same [originalSourceId]. That means that if the same manga
+ * (from the user perspective) is marked as favorite from multiples sources,
+ * it will appears twice in the local source.
+ */
+interface Manga {
+    var name: String
+
+    /**
+     * The source to which the manga need to be loaded to load all wanted information.
+     * This field is for loading, and not for display.
+     */
+    var sourceId: Int
+    /**
+     * The original source of the manga. This field is use for display, to let the user know
+     * from which source the manga have been retrieved first.
+     */
+    var originalSourceId: Int
+    /**
+     * The url used to retrieve the original manga information. It have multiple use:
+     * - for website source, know where to load all manga information.
+     * - for local source, where the manga can be loaded to have up-to-date information.
+     */
+    var url: String
+
+    var coverUrl: String?
+
+    var summary: String?
+
+    var authors: List<String>?
+
+    var genres: List<String>?
+    /**
+     * Rating of the manga, between 0.0 and 1.0 (both included).
+     */
+    var rating: Float?
+    /**
+     * Manga current status. If not already loaded, use [Status.Unknown]
+     */
+    var status: Status
+
+    /*
+     * Under this comment all fields are fields that need to be loaded from
+     * the database (local source) to know their values.
+     *
+     * Putting those fields here an not in a subclass allows for every source
+     * (including website) to know if a manga have been marked as favorite or
+     * have been downloaded for instance.
+     */
+
+    /**
+     * If true, new chapters of this manga will be periodically checked,
+     * and in function of [automaticDownload], will do the appropriate action.
+     */
+    var favorite: Boolean?
+    /**
+     * When a new chapter of this manga is released (and [favorite] is True):
+     * if True: download the chapter automatically
+     * if False: a Notification will be sent to the user to let him know of the release.
+     * TODO: do those features
+     */
+    var automaticDownload: Boolean?
+
+    /**
+     * All possible status of a manga.
+     */
+    enum class Status {
+        Unknown,
+        OnGoing,
+        Finished,
+        Licensed
+    }
+}
